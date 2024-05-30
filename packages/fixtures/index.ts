c8561fc8a9bd4178b6f2918762c0bf91436f74e2
Alexander Teague <alex@teague.de>
2024-05-30 16:24:00 +0200

    chore: remove cruft + add preview task

--- packages/fixtures/index.ts
+++
@@ -1,1 +0,0 @@
-export { products } from "./src/products";
--- packages/fixtures/src/products.ts
+++
@@ -1,5 +0,0 @@
-// import { default as products } from "./products.json"
-// import products from "./products.json"
-
-export const products = [];
-// export const products = () => [];
